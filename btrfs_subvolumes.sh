5d149606caa07ea01c126c8a8044a006f413ff43
Eduardo Carvalho <114698339+educarvallho@users.noreply.github.com>
2025-07-19 17:31:43 +0000

    Update btrfs_subvolumes.sh
    Fix

--- btrfs_subvolumes.sh
+++ btrfs_subvolumes.sh
@@ -1,65 +1,57 @@
 #!/bin/bash
 set -e
 
-echo "Iniciando reorganização do Btrfs com subvolumes otimizados..."
+echo "[1/8] Detectando partições EFI e Btrfs..."
 
-# Detecta partição EFI e raiz Btrfs
-EFI_PART=$(blkid | grep "vfat" | cut -d: -f1 | head -n1)
-ROOT_PART=$(blkid | grep "btrfs" | cut -d: -f1 | head -n1)
+EFI_PART=$(lsblk -pn -o NAME,FSTYPE | grep vfat | awk '{print $1}' | head -n1)
+ROOT_PART=$(lsblk -pn -o NAME,FSTYPE | grep btrfs | awk '{print $1}' | head -n1)
 
-# Define pontos de montagem temporários
 OLDROOT="/mnt/oldroot"
 NEWROOT="/mnt/newroot"
-BACKUP="/tmp/backup"
+BACKUP="/tmp/btrfs_backup"
 
-echo "Criando pontos de montagem..."
-mkdir -p $OLDROOT $NEWROOT $BACKUP
+echo "[2/8] Montando volume raiz original..."
+mkdir -p "$OLDROOT"
+mount "$ROOT_PART" "$OLDROOT"
 
-echo "Montando subvolume raiz padrão (subvolid=5) em $OLDROOT..."
-mount -o subvolid=5 $ROOT_PART $OLDROOT
+echo "[3/8] Realizando backup temporário na RAM..."
+rsync -aAXHv --exclude=/tmp_btrfs_backup "$OLDROOT/" "$BACKUP/"
 
-echo "Fazendo backup temporário dos dados para $BACKUP..."
-rsync -aAXHv --exclude=/tmp/backup $OLDROOT/ $BACKUP/
-
-echo "Criando novos subvolumes no volume raiz..."
+echo "[4/8] Criando novos subvolumes no volume raiz..."
 for sub in @ @home @cache @log @tmp @snapshots; do
-  if btrfs subvolume show $OLDROOT/$sub &>/dev/null; then
-    echo "Subvolume $sub já existe, pulando criação."
-  else
-    btrfs subvolume create $OLDROOT/$sub
+    btrfs subvolume create "$OLDROOT/$sub"
     echo "Subvolume $sub criado."
-  fi
 done
 
-echo "Limpando conteúdo do subvolume raiz padrão (subvolid=5)..."
-rm -rf $OLDROOT/*
+echo "[5/8] Limpando conteúdo do subvolume raiz padrão (subvolid=5)..."
+umount "$OLDROOT"
+mount -o subvolid=5 "$ROOT_PART" "$OLDROOT"
+rm -rf "$OLDROOT"/*
 
-echo "Montando novos subvolumes na árvore $NEWROOT..."
-mount -o subvol=@ $ROOT_PART $NEWROOT
-mkdir -p $NEWROOT/{home,var/cache,var/log,var/tmp,.snapshots}
-mount -o subvol=@home      $ROOT_PART $NEWROOT/home
-mount -o subvol=@cache     $ROOT_PART $NEWROOT/var/cache
-mount -o subvol=@log       $ROOT_PART $NEWROOT/var/log
-mount -o subvol=@tmp       $ROOT_PART $NEWROOT/var/tmp
-mount -o subvol=@snapshots $ROOT_PART $NEWROOT/.snapshots
+echo "[6/8] Montando novos subvolumes na árvore $NEWROOT..."
+mkdir -p "$NEWROOT"
+mount -o subvol=@ "$ROOT_PART" "$NEWROOT"
+mkdir -p "$NEWROOT"/{home,var/cache,var/log,var/tmp,.snapshots}
 
-echo "Restaurando dados do backup para os subvolumes..."
-rsync -aAXHv $BACKUP/             $NEWROOT/
-rsync -aAXHv $BACKUP/home/        $NEWROOT/home/
-rsync -aAXHv $BACKUP/var/cache/   $NEWROOT/var/cache/
-rsync -aAXHv $BACKUP/var/log/     $NEWROOT/var/log/
-rsync -aAXHv $BACKUP/var/tmp/     $NEWROOT/var/tmp/
+mount -o subvol=@home      "$ROOT_PART" "$NEWROOT/home"
+mount -o subvol=@cache     "$ROOT_PART" "$NEWROOT/var/cache"
+mount -o subvol=@log       "$ROOT_PART" "$NEWROOT/var/log"
+mount -o subvol=@tmp       "$ROOT_PART" "$NEWROOT/var/tmp"
+mount -o subvol=@snapshots "$ROOT_PART" "$NEWROOT/.snapshots"
 
-echo "Removendo backup temporário..."
-rm -rf $BACKUP
+echo "[7/8] Restaurando backup nos novos subvolumes..."
+rsync -aAXHv "$BACKUP/"             "$NEWROOT/"
+rsync -aAXHv "$BACKUP/home/"        "$NEWROOT/home/"
+rsync -aAXHv "$BACKUP/var/cache/"   "$NEWROOT/var/cache/"
+rsync -aAXHv "$BACKUP/var/log/"     "$NEWROOT/var/log/"
+rsync -aAXHv "$BACKUP/var/tmp/"     "$NEWROOT/var/tmp/"
 
-# Atualiza fstab com UUIDs reais
-EFI_UUID=$(blkid -s UUID -o value $EFI_PART)
-ROOT_UUID=$(blkid -s UUID -o value $ROOT_PART)
+echo "[8/8] Atualizando fstab..."
 
-echo "Atualizando /etc/fstab no novo sistema..."
+EFI_UUID=$(blkid -s UUID -o value "$EFI_PART")
+ROOT_UUID=$(blkid -s UUID -o value "$ROOT_PART")
 
-cat > $NEWROOT/etc/fstab <<EOF
+cat > "$NEWROOT/etc/fstab" <<EOF
 UUID=$EFI_UUID /boot/efi vfat umask=0077 0 1
 UUID=$ROOT_UUID /              btrfs subvol=@,defaults,noatime,space_cache,ssd,autodefrag,discard=async,compress=zstd 0 1
 UUID=$ROOT_UUID /home          btrfs subvol=@home,defaults,noatime,space_cache,ssd,autodefrag,discard=async,compress=zstd 0 0
@@ -68,9 +60,5 @@ UUID=$ROOT_UUID /var/log       btrfs subvol=@log,defaults,noatime,space_cache,ss
 UUID=$ROOT_UUID /var/tmp       btrfs subvol=@tmp,defaults,noatime,space_cache,ssd,autodefrag,discard=async,compress=zstd 0 0
 UUID=$ROOT_UUID /.snapshots    btrfs subvol=@snapshots,defaults,noatime,space_cache,ssd,autodefrag,discard=async,compress=zstd 0 0
 EOF
-
-echo "Desmontando pontos temporários..."
-umount $OLDROOT
-umount $NEWROOT
 
 echo "Sistema reorganizado com subvolumes otimizados e fstab atualizado."
